901f375624b5805cad36e73a549231d4eb146a62
ashishchaudhary66 <ashishkumarchaudhary66@gmail.com>
2024-12-29 23:01:34 +0530

    Add project files.

---
+++ INDWalks.API/Data/INDWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using INDWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace INDWalks.API.Data
+{
+    public class INDWalksDbContext : DbContext
+    {
+        public INDWalksDbContext(DbContextOptions<INDWalksDbContext> options):base(options)
+        {
+            
+        }
+
+        DbSet<Difficulty> Difficulties { get; set; }
+        DbSet<Region> Regions { get; set; }
+        DbSet<Walk> Walks { get; set; }
+    }
+}
